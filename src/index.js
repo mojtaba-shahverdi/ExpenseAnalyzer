26af8b09987b42b3ee70ba78cba8b09c747088ec
moji <mojtaba0shahverdi@gmail.com>
2022-07-19 05:47:05 +0430

    index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from "react-dom/client";
+import { Provider } from './context/context';
+import App from './App'
+import './index.css'
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <Provider>
+        <App />
+    </Provider>
+);
