92a9c3e06e41cc20186a56ce1ba63894606d3842
moji <mojtaba0shahverdi@gmail.com>
2022-07-19 05:48:05 +0430

    context.js

---
+++ src/context/context.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import contextReducer from './contextReducer'
+const initialState = []
+
+export const ExpenseAnalyzerContext = React.createContext(initialState)
+
+export const Provider = ({ children }) => {
+    const [transactions, dispatch] = React.useReducer(contextReducer, initialState)
+
+    const deleteTransaction = (id) => dispatch({ type: 'DELETE_TRANSACTION', payload: id })
+    const addTransaction = (transaction) => dispatch({ type: 'ADD_TRANSACTION', payload: transaction })
+
+
+    return (
+        <ExpenseAnalyzerContext.Provider value={{
+            deleteTransaction, 
+            addTransaction
+        }}>
+            {children}
+        </ExpenseAnalyzerContext.Provider>
+    )
+}
