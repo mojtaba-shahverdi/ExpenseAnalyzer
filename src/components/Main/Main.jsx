2d49853d7d995ae39801f9a3c6e414238e546889
moji <mojtaba0shahverdi@gmail.com>
2022-07-21 04:02:57 +0430

    Main UPDATED

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import { Card, CardContent, CardHeader, Divider, Typography } from '@material-ui/core'
+import { ExpenseAnalyzerContext } from '../../context/context';
+
 import Grid from '@material-ui/core/Grid';
 import useStyles from './styles'
 import Form from './Form/Form';
 import List from './List/List';
+import InfoCard from '../InfoCard';
 
 const Main = () => {
-
     const classes = useStyles()
+    const { balance } = React.useContext(ExpenseAnalyzerContext)
 
   return (
     <Card className={classes.root}>
         <CardHeader title='Expense Analyser' subheader='Powered by Speechly'/>
         <CardContent>
-            <Typography align='center' variant='h5'>Total Balance $100</Typography>
+            <Typography align='center' variant='h5'>Total Balance ${balance}</Typography>
             <Typography variant='subtitle1' style={{ lineHeight: '1.5em', marginTop: '20px' }}>
-                Try saying: Add income for $100 in Category Salary for Monday...
+                <InfoCard />
             </Typography>
-            <Divider/>
+            <Divider className={classes.divider} />
             <Form />
         </CardContent>
         <CardContent className={classes.cardContent}>
