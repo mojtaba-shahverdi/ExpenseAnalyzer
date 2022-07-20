a9c2fab6c138ad0425c3ecec56f0dc046821da6f
moji <mojtaba0shahverdi@gmail.com>
2022-07-21 04:03:15 +0430

    context reducer UPDATED

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -4,9 +4,13 @@ const contextReducer = (state, action) => {
         case 'DELETE_TRANSACTION':
             transactions = state.filter((t) => t.id !== action.payload);
 
+            localStorage.setItem('transactions', JSON.stringify(transactions))
+
             return transactions;
         case 'ADD_TRANSACTION':
             transactions = [action.payload, ...state]
+
+            localStorage.setItem('transactions', JSON.stringify(transactions))
 
             return transactions
         default:
