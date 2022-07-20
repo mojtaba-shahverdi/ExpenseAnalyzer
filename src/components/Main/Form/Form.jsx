b0cb65071eea7c19d5088784c6f27a51028d0802
moji <mojtaba0shahverdi@gmail.com>
2022-07-20 22:02:55 +0430

    Form UPDATED

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -2,20 +2,24 @@ import React from 'react'
 import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core'
 import { ExpenseAnalyzerContext } from '../../../context/context'
 import { v4 as uuidv4 } from 'uuid'
+import { useSpeechContext } from '@speechly/react-client'
+
+import formatDate from '../../../utils/fomatDate'
 import useStyles from './styles'
+import { incomeCategories, expenseCategories } from '../../../constants/categories'
 
 const initialState = {
     amount: '',
     category: '',
     type: 'Income',
-    date: new Date(),
+    date: formatDate(new Date()),
 }
 
 const Form = () => {
     const classes = useStyles()
-
     const [formData, setFormData] = React.useState(initialState)
     const { addTransaction } = React.useContext(ExpenseAnalyzerContext)
+    const { segment } = useSpeechContext()
 
     const createTransaction = () => {
         const transaction = { ...formData, amount: Number(formData.amount), id: uuidv4() }
@@ -24,12 +28,25 @@ const Form = () => {
         setFormData(initialState)
     }
 
-console.log(formData)
+    React.useEffect(() => {
+        if(segment){
+            if(segment.intent.intent === 'add_expense'){
+                setFormData({ ...formData, type: 'Expense' })
+            }else if(segment.intent.intent ===  'add_income'){
+                setFormData({...formData, type: 'Income'})
+            }else if(segment.isFinal && segment.intent.intent === 'create_transaction'){
+                return createTransaction()
+            }
+        }
+    }, [segment])
+
+    const selectedCategories = formData.type === 'Income' ? incomeCategories : expenseCategories
+
     return (
         <Grid container spacing={2}>
             <Grid item xs={12}>
-                <Typography align='center' variant='subtitle2' gutterBottom>
-                    ...
+                <Typography align='center' variant='p' gutterBottom>
+                    {segment && segment.words.map((w) => w.value).join(' ')}
                 </Typography>
             </Grid>
             <Grid item xs={6}>
@@ -45,8 +62,7 @@ console.log(formData)
                 <FormControl fullWidth>
                     <InputLabel>Category</InputLabel>
                     <Select value={formData.category} onChange={(e) => setFormData({ ...formData, category: e.target.value })}>
-                        <MenuItem value='business'>Business</MenuItem>
-                        <MenuItem value='salary'>Salary</MenuItem>
+                        {selectedCategories.map((c) => <MenuItem key={c.type} value={c.type}>{c.type}</MenuItem>)}
                     </Select>
                 </FormControl>
             </Grid>
@@ -54,7 +70,7 @@ console.log(formData)
                 <TextField type='number' label='Amount' fullWidth value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value })} />
             </Grid>
             <Grid item xs={6}>
-                <TextField type='date' label='Date' fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: e.target.value })} />
+                <TextField type='date' label='Date' fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: formatDate(e.target.value) })} />
             </Grid>
             <Button className={classes.button} variant='outlined' color='primary' fullWidth onClick={createTransaction}>Create</Button>
         </Grid>
