4ef9b7a988c568ee07ea850481a7deeb83630409
moji <mojtaba0shahverdi@gmail.com>
2022-07-21 04:01:49 +0430

    App UPDATED

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid';
-import { SpeechState, useSpeechContext } from "@speechly/react-client";
 import { PushToTalkButton, PushToTalkButtonContainer } from '@speechly/react-ui';
-import {
-    BigTranscript,
-    BigTranscriptContainer,
-    ErrorPanel
-} from "@speechly/react-ui";
-import {
-    SpeechProvider
-} from "@speechly/react-client";
+import { BigTranscript, BigTranscriptContainer } from "@speechly/react-ui";
 import Main from './components/Main/Main';
 import Details from './components/Details/Details'
 import useStyles from './styles'
@@ -20,25 +12,23 @@ const App = () => {
     return (
         <div>
             <Grid className={classes.grid} container spacing={0} alignItems='center' justifyContent='center' style={{ height: '100vh' }}>
-                <Grid item xs={12} sm={4}>
-                    <Details title='Income' />
-                </Grid>
-                <Grid item xs={12} sm={3}>
+                <Grid item xs={12} sm={3} className={classes.main}>
                     <Main />
                 </Grid>
-                <Grid item xs={12} sm={4}>
+                <Grid item xs={12} sm={4} className={classes.income}>
+                    <Details title='Income' />
+                </Grid>
+                <Grid item xs={12} sm={4} className={classes.expense}>
                     <Details title='Expense' />
                 </Grid>
             </Grid>
-            {/* <SpeechProvider appId="b47aaa8c-8edb-4639-ac96-feaca267063d" language="en-US"> */}
-                <BigTranscriptContainer>
-                    <BigTranscript />
-                </BigTranscriptContainer>
+            <BigTranscriptContainer>
+                <BigTranscript />
+            </BigTranscriptContainer>
 
-                <PushToTalkButtonContainer>
-                    <PushToTalkButton captureKey=" " />
-                </PushToTalkButtonContainer>
-            {/* </SpeechProvider> */}
+            <PushToTalkButtonContainer>
+                <PushToTalkButton captureKey=" " />
+            </PushToTalkButtonContainer>
         </div>
     )
 }
