ddd278b5dce87f6a19ce63cc17bc48cbfc757f67
moji <mojtaba0shahverdi@gmail.com>
2022-07-20 22:02:10 +0430

    index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { SpeechProvider } from '@speechly/react-client';
+
 import { Provider } from './context/context';
 import App from './App'
 import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-    <Provider>
-        <App />
-    </Provider>
+ReactDOM.render(
+    <SpeechProvider appId="b47aaa8c-8edb-4639-ac96-feaca267063d" language="en-US">
+        <Provider>
+            <App />
+        </Provider>,
+    </SpeechProvider>,
+    document.getElementById('root')
 );
